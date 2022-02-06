3b5cd5c697ea70b5669ca1403c822e04cf85162b
cutiefunny <cutiefunny@gmail.com>
2022-02-06 20:32:07 +0900

    split pc/mobile

--- router.js
+++ router.js
@@ -3,12 +3,20 @@ const fs = require('fs');
 
 exports.main = function(req,res) {
 
+    var MobileDetect = require('mobile-detect'),
+    md = new MobileDetect(req.headers['user-agent']);
+    
+    //pc/mobile 구분
+    var target="main";
+    if(!md.mobile()) target="main_pc";
+
     CRUD.searchData("getAll","products").then(products=>{
         console.log(products);
-        res.render('main', { 
+        res.render(target, { 
             title: '근육고양이 잡화점'
             , products : products
             , name : '김냐냐'
+            , device : md.mobile()
         });
     });
     // fs.readdir(__dirname+'/images/', function(err,folderList){
