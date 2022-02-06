666fcfe3b004e5cea262a0f80bfdd58a41ff4488
cutiefunny <cutiefunny@gmail.com>
2022-02-06 23:49:40 +0900

    tag

--- script/main.js
+++ script/main.js
@@ -18,3 +18,22 @@ function modal(product){
     document.getElementById("detail").innerHTML="";
     callAjax("getDetail",product);
 }
+
+function clickTag(id){
+    document.getElementsByName("tag").forEach(tag=> {
+        if(tag.classList.contains("green")) tag.classList.remove("green");
+    });
+    document.getElementById(id).classList.add("green");
+
+    if(id=="전체") {
+        document.getElementsByName("box").forEach(box=>{
+            box.setAttribute("style","display:block");
+        });
+    }
+    else{
+        document.getElementsByName("box").forEach(box=>{
+            box.setAttribute("style","display:none");
+            if(box.getAttribute("id").includes(id)) box.setAttribute("style","display:block");
+        });
+    }
+}
