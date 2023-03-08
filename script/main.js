bc63672e34b429e432a8ece4b64d78b4e3cf0f27
cutiefunny <cutiefunny@gmail.com>
2023-03-08 15:57:31 +0900

    임시

--- script/main.js
+++ script/main.js
@@ -141,27 +141,3 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
-
-  const swiper2 = new Swiper('.swiper2', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    autoHeight : true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
